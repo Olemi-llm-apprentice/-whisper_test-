13fce436363e3aee6074a12d35d1077368d9e956
Olemi <139845365+Olemi-llm-apprentice@users.noreply.github.com>
2023-10-27 00:16:10 +0900

    Add files via upload

---
+++ test_summarizd.py
@@ -0,0 +1,84 @@
+import os
+import time
+import openai
+import tkinter as tk
+from tkinter import filedialog
+from dotenv import load_dotenv, find_dotenv
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.docstore.document import Document
+
+start_time = time.time()
+
+load_dotenv()
+openai_api_key = os.environ['OPENAI_API_KEY']
+
+# ファイル選択ダイアログ
+root = tk.Tk()
+root.withdraw()
+file_path = filedialog.askopenfilename()
+with open(file_path, 'r') as f:
+    long_text = f.read()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000)
+texts = text_splitter.split_text(long_text)
+docs = [Document(page_content=t) for t in texts]
+
+# docs リストの上から5つを見やすく表示
+# for i, doc in enumerate(docs[:5]):
+#     print(f"Document {i + 1}: {doc.page_content[:50]}...")  # 最初の50文字だけ表示
+ 
+prompt1='''
+Take a deep breath and work on this problem step-by-step.
+
+#Role.
+You are able to see through the overall structure and are a genius at summarizing sentences to order.
+#Conditions
+The following sentences are text data transcribed from audio that has been divided into multiple chunks.
+The first chunk of text is the first sentence.
+Please make sure to output the data in Japanese.
+Please output only the summary content and do not speak any other language.
+#Order
+Please summarize the following sentences in Japanese without any missing content.
+
+#First sentence:
+'''
+
+prompt2=f'''
+Take a deep breath and work on this problem step-by-step.
+
+#Role.
+You are able to see through the overall structure and are a genius at summarizing sentences to order.
+#Conditions
+The following sentences are text data transcribed from audio that has been divided into multiple chunks.
+Please understand the content of (one previous summary result) and summarize only the sentences that follow.
+Please be sure to cut out any overlap between the content of the previous summary and the content of the following sentences.
+The final output should be based on the output of all chunks of the summary results as they will be combined.
+Please make sure to output the data in Japanese.
+Please output only the summary content and do not speak any other language.
+#One previous summary result:
+
+#Order
+Please summarize the following sentences in Japanese without any missing content.
+
+#Continued sentences:
+'''
+
+
+    
+for i, doc in enumerate(docs[:5]):
+    if i == 0:
+        response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+        {"role": "system", "content": prompt1},
+        {"role": "user", "content": doc.page_content}        
+    ]
+        summary = response["choices"][0]["message"]["content"]
+)
+        
+        prompt = "prompt1"
+        
+    else:
+        prompt = "prompt2"
+
+print(f"{time.time() - start_time}")
